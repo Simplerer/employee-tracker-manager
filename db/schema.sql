7cdc02d4286fbec78e587d90e28473cf95becb85
A Stone <simplererstone@gmail.com>
2023-01-10 00:32:10 -0500

    lost in data land, figured out joining and working on adding employee

--- db/schema.sql
+++ db/schema.sql
@@ -9,8 +9,11 @@ CREATE Table departments (
 CREATE Table roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT
+    salary DECIMAL NOT NULL,    
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
     );
     
 CREATE Table employees (
@@ -18,5 +21,8 @@ CREATE Table employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
 );
