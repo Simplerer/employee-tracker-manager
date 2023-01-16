5761a171205f9d0c85bbf8182b881fe43fb77bad
A Stone <simplererstone@gmail.com>
2023-01-16 16:29:55 -0500

    moved around mysql connections and added instructions on how to load in databsae for app

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const { welcome } = require('./lib/prompt');
+const mysql = require('mysql2');
 
-//Initializer of program, hands off to module
-welcome();
+const db = mysql.createConnection({
+  user: 'root',
+  database: 'employees_db'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log('MAKE SURE DATABASE IS INSTALLED BY RUNNING `mysql -uroot` then `source db/schema.sql` and finally `source db/seeds.sql`');
+  } else {
+    welcome(db);
+  }
+});
